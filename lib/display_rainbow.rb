f9e62bd51a863aa320cf0ac92010cc671f11a081
Matt Hough <matt@hough.tv>
2017-09-03 20:26:23 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,8 @@
 # Write your #display_rainbow method here
 rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
+rbw_colors = 3
+
 puts rainbow[0]
 
 #def display_rainbow(rbw_colors)
